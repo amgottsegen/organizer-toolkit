d9981b018146615d441872707796d52f0d07dc85
Amy Gottsegen <a.m.gottsegen@gmail.com>
2026-03-11 22:38:53 +0000

    added fields and validation to OT Event, as well as linked OT Event Space doctype

--- organizer_toolkit/organizer_toolkit/doctype/ot_event/ot_event.py
+++ organizer_toolkit/organizer_toolkit/doctype/ot_event/ot_event.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2026, CREATE Lab and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import add_to_date
 
 
 class OTEvent(Document):
+	def validate(self):
+		self.event_end = add_to_date(self.event_start, seconds=self.event_duration)
+
 	pass
---
+++ organizer_toolkit/organizer_toolkit/doctype/ot_event_space/test_ot_event_space.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, CREATE Lab and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOTEventSpace(FrappeTestCase):
+	pass
