b165a8829d74e0faf86fb48080951909b6c9748c
Amy Gottsegen <a.m.gottsegen@gmail.com>
2026-03-11 18:43:43 +0000

    enable empty phone number while keeping default country code and add OT Language docty pe

--- organizer_toolkit/hooks.py
+++ organizer_toolkit/hooks.py
@@ -5,7 +5,7 @@ app_description = "Tools for organizing constituents"
 app_email = "iglesiascrmtech@gmail.com"
 app_license = "mit"
 
-fixtures = ["Client Script", "Number Card"]
+fixtures = ["Client Script", "Number Card", {"dt": "OT Language"}]
 
 # Apps
 # ------------------
--- organizer_toolkit/organizer_toolkit/doctype/ot_constituent/ot_constituent.py
+++ organizer_toolkit/organizer_toolkit/doctype/ot_constituent/ot_constituent.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2026, CREATE Lab and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class OTConstituent(Document):
+	def validate(self):
+		if self.mobile_phone == "+1-":
+			self.mobile_phone = None
+
+		if self.home_phone == "+1-":
+			self.home_phone = None
+
 	pass
