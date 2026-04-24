61a5a5005fe7e45e2d5354e072182f8c9249df9f
Abhinav2656 <abhinavpandey1037@gmail.com>
2026-04-25 01:40:41 +0530

    fix(event-space): clear invalid default phone number to resolve validation failure

--- organizer_toolkit/organizer_toolkit/doctype/ot_constituent/ot_constituent_list.js
+++ organizer_toolkit/organizer_toolkit/doctype/ot_constituent/ot_constituent_list.js
@@ -2,6 +2,6 @@
 // For license information, please see license.txt
 
 frappe.listview_settings["OT Constituent"] = {
-    hide_name_column: true,
-    hide_name_filter: true
+	hide_name_column: true,
+	hide_name_filter: true,
 };
